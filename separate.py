65e001e9e7d7124141ef922a6e41ac7eae37f77e
Alexey Slizkov <elexunix@gmail.com>
2024-06-22 01:37:25 +0300

    Loss 0.045

--- separate.py
+++ separate.py
@@ -1,3 +1,4 @@
+import sys
 import numpy as np
 import torch, torch.nn as nn, torch.nn.functional as F
 from tqdm import tqdm, trange
@@ -8,6 +9,7 @@ class Dataset(nn.Module):
     self.l = open(filename).readlines()
     self.l, n = self.l[:-1], int(self.l[-1])
     assert len(self.l) == n
+    n = 300_000
     if split == 'train':
       self.l = self.l[:9 * n // 10]
     elif split == 'test':
@@ -21,8 +23,9 @@ class Dataset(nn.Module):
   def __len__(self):
     return len(self.l)
 
-train_dataset = Dataset('collatz_dataset_N1000000_L100', split='train')
-test_dataset = Dataset('collatz_dataset_N1000000_L100', split='test')
+filename = sys.argv[1]
+train_dataset = Dataset(filename, split='train')
+test_dataset = Dataset(filename, split='test')
 
 def collate_fn(batch):
   L = max(max(t.shape[-1], s.shape[-1]) for t, s in batch)
@@ -30,7 +33,7 @@ def collate_fn(batch):
            torch.stack([F.pad(s, (L - s.shape[-1], 0)) for t, s in batch]))
   return batch
 
-batch_size = 16
+batch_size = 32
 train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size, collate_fn=collate_fn, shuffle=True, pin_memory=True)
 test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size, collate_fn=collate_fn, shuffle=True, pin_memory=True)
 
@@ -55,20 +58,30 @@ class Model(nn.Module):
 device = 'cuda'
 H = Model(n_channels=512, embedding_dim=256).to(device)  # if it's energy, why don't we call it "H"?))
 optimizer = torch.optim.Adam(H.parameters(), lr=1e-3)
+#scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=1-5/len(train_dataloader))  # e^5 ~= 150
+scheduler = torch.optim.lr_scheduler.LinearLR(optimizer, 1, 0, total_iters=len(train_dataloader))
 
 losses = []
 for s, t in tqdm(train_dataloader):
   s, t = s.to(device), t.to(device)
   E_s, E_t = H(s), H(t)
+  if np.random.randint(10000) == 0:
+    print(f'{E_s=}, {E_t=}')
   loss = F.relu(E_t + 1 - E_s).mean()
   optimizer.zero_grad()
   loss.backward()
   optimizer.step()
   losses.append(loss.item())
-print('Loss:', np.mean(losses[-100:]))
+  if scheduler is not None:
+    scheduler.step()
+losses = np.array(losses)
+print('Loss:', losses[-100:].mean())
+
+k = len(losses) // 100 * 100
+losses = losses[:k].reshape(k // 100, 100).mean(-1)
 
 plt.figure(figsize=(19.2, 10.8))
 plt.plot(losses)
-plt.xlabel('step')
+plt.xlabel('step, 100')
 plt.ylabel('loss')
 plt.savefig('loss.png')
